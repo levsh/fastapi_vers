f14599b094f8c1abbf603f196ec40c6ccdcdcab7
Roman Koshel <roma.koshel@gmail.com>
2021-08-15 18:57:36 +0300

    Bump, setup.py

--- fastapi_vers.py
+++ fastapi_vers.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from packaging.version import Version
 
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 
 
 def merge_dicts(src: dict, dst: dict) -> dict:
--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
 from setuptools import setup
 
 
+def get_version():
+    with open("fastapi_vers.py", "r") as f:
+        for line in f.readlines():
+            if line.startswith("__version__ = "):
+                return line.split("=")[1].strip().strip('"')
+    raise Exception("Can't read version")
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name="fastapi_vers",
-    version="0.1.3",
+    version=get_version(),
     author="Roma Koshel",
     author_email="roma.koshel@gmail.com",
+    description="FastAPI versioning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     py_modules=["fastapi_vers"],
     include_package_data=True,
